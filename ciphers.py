5579538c6de25fb9582a28cd11489d1663bfc146
davidposs <davidp525@gmail.com>
2017-03-09 01:01:43 -0800

    Ciphers.py now takes command line argumnets

--- ciphers.py
+++ ciphers.py
@@ -1,3 +1,4 @@
+from Railfence import RailfenceCipher
 from vig import VigenereCipher
 from caesar import CaesarCipher
 from RowTransposition import RowTransposition
@@ -5,28 +6,58 @@ import argparse
 
 # """ Main function for the cipher programs """
 
-"""
+
 def main():
 #    main function
     parser = argparse.ArgumentParser(description = "Multi-cipher encryption program")
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argumet('-e', dest = 'encrypt', action='store_true', help='Give a file to encrypt')
+    group.add_argument('-e', dest = 'encrypt', action='store_true', help='Give a file to encrypt')
     group.add_argument('-d', dest = 'decrypt', action='store_true', help = 'Give a file to decrypt')
+    parser.add_argument('cipher', type = str, nargs = 1, help = 'cipher to use')
     parser.add_argument('key', type = str, nargs ='?', help = 'key for encoding')
     parser.add_argument('filename', type = str, help = 'The file to encrypt or decrypt')
 
     args = parser.parse_args()
-"""
+    key = args.key
+    cipher = args.cipher[0]
+    filename = args.filename
 
-#vig = VigenereCipher('LEMON')
-#vig.set_key('plaintext.txt')
-#print vig.key + '\n'
-#vig.encrypt('plaintext.txt')
-#vig.decrypt('vig-encrypted.txt')
+    if args.encrypt:
+            if cipher == 'vig':
+                vig = VigenereCipher(key)
+                vig.encrypt(filename)
+            elif cipher == 'ces':
+                ces = CaesarCipher(key)
+                ces.encrypt(filename)
+            elif cipher == 'rtc':
+                rtc = RowTransposition(key)
+                rtc.encrypt(filename)
+            elif cipher == 'rfc':
+                rfc = RailfenceCipher(key)
+                rfc.encrypt(filename)
+            elif cipher == 'pfc':
+                print "I haven't done playfair yet"
+                return
 
-#ces = CaesarCipher(8)
-#ces.encrypt('plaintext.txt')
-#ces.decrypt('ces-encrypted.txt')
+    elif args.decrypt:
+            if cipher == 'vig':
+                vig = VigenereCipher(key)
+                vig.decrypt(filename)
+            elif cipher == ['ces']:
+                ces = CaesarCipher(key)
+                ces.decrypt(filename)
+            elif cipher == 'rtc':
+                rtc = RowTransposition(key)
+                rts.decrypt(filename)
+            elif cipher == 'rfc':
+                rfc = RailfenceCipher(key)
+                rfc.decrypt(filename)
+            elif cipher == 'pfc':
+                print "Still haven't done playfair :( "
+                return
 
-rtp = RowTransposition('4321')
-rtp.encrypt('plaintext.txt')
+    exit()
+
+if __name__ == "__main__":
+    main()
+
